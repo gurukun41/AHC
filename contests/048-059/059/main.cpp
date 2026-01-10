98a5859f71d285d989cabda9f53ec993f6f142c2
gurukun41 <kouga1582@gmail.com>
2026-01-10 16:01:45 +0900

    巡回山登り貪欲と大差なし

--- contests/048-059/059/main.cpp
+++ contests/048-059/059/main.cpp
@@ -43,85 +43,134 @@ ll N = 20;
 vvl a(20, vl(20));
 
 
+ll dist(pl p1, pl p2) {
+    return abs(p1.first - p2.first) + abs(p1.second - p2.second);
+}
+
+struct Task {
+    ll val;
+    pl p[2];
+};
+
 void solve() {
-    vector<char> moves; // ターンごとの行動 U, D, L, Rで移動、Zで取る、Xで置く
-    ll i = 0, j = 0; // 現在位置
-    stack<ll> st; // 山札
-    vvl now = a; // 現在の盤面
-    ll count = 0; // 取ったカードの組み数
-    ll turn = 0; // ターン数
-
-    pl target = {-1,-1}; // 目標位置
-    while (turn < 16000 && count < 200) {
-        if(target != pl{-1,-1} && target != pl{i,j}){
-            // 目標位置に向かう
-            if(target.first < i){
-                moves.push_back('U');
-                i--;
-            } else if(target.first > i){
-                moves.push_back('D');
-                i++;
-            } else if(target.second < j){
-                moves.push_back('L');
-                j--;
-            } else if(target.second > j){
-                moves.push_back('R');
-                j++;
-            }
-            turn++;
-            continue;
-        } else if(target != pl{-1,-1} && target == pl{i,j}){
-            target = pl{-1,-1};
-            moves.push_back('Z');
-            if(!st.empty() && st.top() == now[i][j]){
-                st.pop();
-                count++;
-            } else {
-                st.push(now[i][j]);
-            }
-            now[i][j] = -1;
-            turn++;
-            continue;
-        }
-
-        if(st.empty()) {
-            ll min_dist = 1e9;
-            pl best = {-1, -1};
-            rep(ni,0,N) rep(nj,0,N) {
-                if(now[ni][nj] != -1) {
-                    ll dist = abs(i - ni) + abs(j - nj);
-                    if(dist < min_dist) {
-                        min_dist = dist;
-                        best = pl{ni, nj};
-                    }
-                }
-            }
-            target = best;
-        } else {
-            rep(k,0,(N-1)*(N-1)+1){
-                rep(ni,0,k+1){
-                    if(0<=ni && ni<N && 0<=k-ni && k-ni<N ){
-                        if(now[ni][k-ni] == st.top()){
-                            target = pl{ni,k-ni};
-                            break;
-                        }
-                    }
-                }
-                if(target != pl{-1,-1}){
-                    break;
-                }
-            }
+    map<ll, vpl> m;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (a[i][j] != -1) m[a[i][j]].push_back({(ll)i, (ll)j});
         }
     }
 
-    rep(index,0,moves.size()) {
-        cout << moves[index] << "\n";
+    vector<Task> tasks;
+    for (auto const& [val, pos] : m) {
+        if (pos.size() == 2) {
+            tasks.push_back({val, {pos[0], pos[1]}});
+        }
+    }
+
+    int M = tasks.size();
+    if (M == 0) return;
+
+    vl order;
+    vl ori;
+    vb used(M, false);
+    pl cur = {0, 0};
+
+    for (int i = 0; i < M; i++) {
+        int best_idx = -1;
+        ll min_d = 1e18;
+        int best_o = 0;
+        for (int j = 0; j < M; j++) {
+            if (used[j]) continue;
+            for (int o = 0; o < 2; o++) {
+                ll d = dist(cur, tasks[j].p[o]);
+                if (d < min_d) {
+                    min_d = d;
+                    best_idx = j;
+                    best_o = o;
+                }
+            }
+        }
+        used[best_idx] = true;
+        order.push_back(best_idx);
+        ori.push_back(best_o);
+        cur = tasks[best_idx].p[1 - best_o];
+    }
+
+    auto get_total_dist = [&]() {
+        ll total = 0;
+        pl p_prev = {0, 0};
+        for (int i = 0; i < M; i++) {
+            total += dist(p_prev, tasks[order[i]].p[ori[i]]);
+            total += dist(tasks[order[i]].p[ori[i]], tasks[order[i]].p[1 - ori[i]]);
+            p_prev = tasks[order[i]].p[1 - ori[i]];
+        }
+        return total;
+    };
+
+    ll current_dist = get_total_dist();
+    mt19937 engine(42);
+    auto start_time = chrono::steady_clock::now();
+
+    while (true) {
+        auto now_time = chrono::steady_clock::now();
+        if (chrono::duration_cast<chrono::milliseconds>(now_time - start_time).count() > 1800) break;
+
+        int type = engine() % 100;
+        if (type < 30) {
+            int i = engine() % M;
+            int j = engine() % M;
+            if (i == j) continue;
+            swap(order[i], order[j]);
+            ll next_dist = get_total_dist();
+            if (next_dist < current_dist) current_dist = next_dist;
+            else swap(order[i], order[j]);
+        } else if (type < 60) {
+            int i = engine() % M;
+            ori[i] = 1 - ori[i];
+            ll next_dist = get_total_dist();
+            if (next_dist < current_dist) current_dist = next_dist;
+            else ori[i] = 1 - ori[i];
+        } else {
+            int i = engine() % M;
+            int j = engine() % M;
+            if (i > j) swap(i, j);
+            if (i == j) continue;
+            reverse(order.begin() + i, order.begin() + j + 1);
+            ll next_dist = get_total_dist();
+            if (next_dist < current_dist) current_dist = next_dist;
+            else reverse(order.begin() + i, order.begin() + j + 1);
+        }
+    }
+
+    auto move_to = [&](pl target, pl& now, vector<char>& res) {
+        while (now.first < target.first) { res.push_back('D'); now.first++; }
+        while (now.first > target.first) { res.push_back('U'); now.first--; }
+        while (now.second < target.second) { res.push_back('R'); now.second++; }
+        while (now.second > target.second) { res.push_back('L'); now.second--; }
+    };
+
+    vector<char> moves;
+    pl now_p = {0, 0};
+    for (int i = 0; i < M; i++) {
+        move_to(tasks[order[i]].p[ori[i]], now_p, moves);
+        moves.push_back('Z');
+        move_to(tasks[order[i]].p[1 - ori[i]], now_p, moves);
+        moves.push_back('Z');
+    }
+
+    for (int i = 0; i < (int)moves.size() && i < 16000; i++) {
+        cout << moves[i] << "\n";
     }
 }
 
-int main(){
+int main() {
     cin >> N;
-    a.resize(N, vl(N));
-    rep(i,0,N)rep(j,0,N) cin >> a[i][j];
+    a.assign(N, vector<ll>(N));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> a[i][j];
+        }
+    }
     solve();
+    return 0;
 }
